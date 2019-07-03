15de1d02114902183da7e47adc75db87bb4a4490
Unknown <oleshkevich19990802@mail.ru>
2019-07-03 23:51:09 +0300

    первые кнопки в меню

---
+++ CloniumTest/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+	public Text CountPlayers;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void UpCount()
+	{
+		int count = int.Parse(CountPlayers.text.Split()[2]);
+		count = Math.Min(12, count + 1);
+		CountPlayers.text = "count players " + count.ToString();
+	}
+
+	public void DownCount()
+	{
+
+		int count = int.Parse(CountPlayers.text.Split()[2]);
+		count = Math.Max(2, count - 1);
+		CountPlayers.text = "count players " + count.ToString();
+	}
+}
